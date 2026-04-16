2698cb4629a9945876e5decdf1f75bc6bd36428a
Claudio Barlassina <claudio_barlassina_ar@hotmail.com>
2026-04-16 16:01:29 -0300

    cambios

--- Front-End/Ferreteria/src/App.jsx
+++ Front-End/Ferreteria/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
-import Success from "./pages/Success"
+
 import useAuthListener from "./components/LayoudShopLogin/hooks/useAuthListerner" 
 import { use } from "react"
 
@@ -16,8 +16,6 @@ useAuthListener() // 🔐 Escucha cambios de autenticación
 
        <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/success" element={<Success />} />
-     
       <Route path="/productos" element={"<Productos />"} />
       <Route path="/carrito" element={"<Carrito />"} />
       <Route path="/producto/:id" element={"<Detalle />"} />
