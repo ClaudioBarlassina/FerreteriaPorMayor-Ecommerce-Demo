3838fdbba2564df36b636fe9553c2fd8edfa6ad1
Claudio Barlassina <claudio_barlassina_ar@hotmail.com>
2026-04-16 16:08:15 -0300

    urlStripe

--- Front-End/Ferreteria/src/components/StripeFront/CheckoutButton.jsx
+++ Front-End/Ferreteria/src/components/StripeFront/CheckoutButton.jsx
@@ -4,7 +4,7 @@ export default function CheckoutButton({ productos, user }) {
   const handleCheckout = async () => {
     try {
     
-      const response = await fetch("https://full-stack-ecommerce-d1a4.onrender.com/api/payment/create-checkout-session", {
+      const response = await fetch("https://ferreteriapormayor-ecommerce-demo.onrender.com/api/payment/create-checkout-session", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
